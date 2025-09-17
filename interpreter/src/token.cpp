a30014ba05f6e42866c7fd3c84cedc6e2edbafd5
Tinkinazoo <127057803+Tinkinazoo@users.noreply.github.com>
2025-09-17 18:48:23 +0300

    Create token.cpp

---
+++ interpreter/src/token.cpp
@@ -0,0 +1,59 @@
+#include "token.h"
+#include <map>
+#include <stdexcept>
+
+Token::Token(TokenType type, const std::string& lexeme, int line)
+    : type(type), lexeme(lexeme), line(line) {}
+
+std::ostream& operator<<(std::ostream& os, const Token& token) {
+    os << "Token(" << tokenTypeToString(token.type) 
+       << ", '" << token.lexeme << "', line: " << token.line << ")";
+    return os;
+}
+
+std::string tokenTypeToString(TokenType type) {
+    static const std::map<TokenType, std::string> typeNames = {
+        {TokenType::LET, "LET"},
+        {TokenType::IF, "IF"},
+        {TokenType::ELSE, "ELSE"},
+        {TokenType::WHILE, "WHILE"},
+        {TokenType::FOR, "FOR"},
+        {TokenType::FUN, "FUN"},
+        {TokenType::RETURN, "RETURN"},
+        {TokenType::PRINT, "PRINT"},
+        {TokenType::TRUE, "TRUE"},
+        {TokenType::FALSE, "FALSE"},
+        {TokenType::AND, "AND"},
+        {TokenType::OR, "OR"},
+        {TokenType::NOT, "NOT"},
+        {TokenType::IDENTIFIER, "IDENTIFIER"},
+        {TokenType::NUMBER, "NUMBER"},
+        {TokenType::STRING, "STRING"},
+        {TokenType::PLUS, "PLUS"},
+        {TokenType::MINUS, "MINUS"},
+        {TokenType::MULTIPLY, "MULTIPLY"},
+        {TokenType::DIVIDE, "DIVIDE"},
+        {TokenType::ASSIGN, "ASSIGN"},
+        {TokenType::EQUALS, "EQUALS"},
+        {TokenType::NOT_EQUALS, "NOT_EQUALS"},
+        {TokenType::LESS, "LESS"},
+        {TokenType::LESS_EQUAL, "LESS_EQUAL"},
+        {TokenType::GREATER, "GREATER"},
+        {TokenType::GREATER_EQUAL, "GREATER_EQUAL"},
+        {TokenType::LEFT_PAREN, "LEFT_PAREN"},
+        {TokenType::RIGHT_PAREN, "RIGHT_PAREN"},
+        {TokenType::LEFT_BRACE, "LEFT_BRACE"},
+        {TokenType::RIGHT_BRACE, "RIGHT_BRACE"},
+        {TokenType::COMMA, "COMMA"},
+        {TokenType::SEMICOLON, "SEMICOLON"},
+        {TokenType::END_OF_FILE, "END_OF_FILE"},
+        {TokenType::ERROR, "ERROR"}
+    };
+    
+    auto it = typeNames.find(type);
+    if (it != typeNames.end()) {
+        return it->second;
+    }
+    
+    throw std::runtime_error("Unknown token type");
+}
